60c7fee01327f52a328d2d867d4afb0a0a10fa6a
Augustus Mocha <mocha4pf@gmail.com>
2024-09-25 13:33:46 +0300

    final

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// Variable to multiply num1 and num2 to get 62
+let num1 = 31; // Example value
+let num2 = 2;  // Example value
+let multiply = num1 * num2; // 31 * 2 = 62
 
-// Write your code here
+// Variable to generate a random integer greater than 0
+let random =  Math.floor(Math.random() * 100) + 1;
+
+
+// Variable to calculate the remainder of num3 divided by num4, with remainder 4
+let num3 = 24; // Example value
+let num4 = 5;  // Example value
+let mod = num3 % num4; // 24 % 5 = 4
+
+
+let max 
+max = Math.max(1,2,3,20)
+console.log(max)
